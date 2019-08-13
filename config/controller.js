de83206aaba6d1e6398cca59ff9dfdcf1165dd03
Kim Helen Quach <KimHelenQuach@kims-air-2.attlocal.net>
2019-08-12 22:08:58 -0700

    final commit

--- config/controller.js
+++ config/controller.js
@@ -1,24 +1,42 @@
-var sql = require('mysql');
-var connection;
+var mysql = require("mysql");
 
-if (process.env.burgersDB_URL) {
-    connection = mysql.createConnection(process.env.burgersDB_URL);
-} 
-else
-{
-    connection= mysql.createConnection({
-        host: 'localholst',
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "",
+//   database: "cat_db"
+// });
+
+// Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+// Export connection for our ORM to use.
+// module.exports = connection;
+
+if (process.env.burgDB_URL) {
+    connection = mysql.createConnection(process.env.burgDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
         user: 'root',
+        port: 3306,
         password: 'Khelenq14',
-        database: 'burgersDB'
+        database: 'burgDB'
     })
 };
 
-connection.connect(function (err){
+connection.connect(function (err) {
     if (err) {
         console.error('error connecting' + err.stack);
         return;
     }
-})
+});
 
 module.exports = connection;
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+app.use(express.static("public"));
+
+app.use((express.urlencoded({ extended: true })));
+app.use(express.json());
+
+var exphbs = require ('express-handlebars');
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+
+var routes = require("./controllers/burg-controllers");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
